19c67347530ebfed0255cc20d8bf78455650ca3c
David Hinkes <david.hinkes@gmail.com>
2012-01-23 23:54:19 +0000

    more hlint cleanup

--- src/Graphics/CR2.hs
+++ src/Graphics/CR2.hs
@@ -3,22 +3,22 @@ module Graphics.CR2
   getCR2Attributes,
   ) where
 
-import qualified Data.ByteString.Lazy as B
-import Data.ByteString.Internal (w2c)
 import Data.Maybe
-import Data.String
 import Data.Serialize.Get
 import Data.Word
 import Util
 
 data Endian = BigEndian | LittleEndian
 
+e8 :: Endian -> Get Word8
 e8 _ = getWord8
 
+e16 :: Endian -> Get Word16
 e16 endian = case endian of
                BigEndian -> getWord16be
                LittleEndian -> getWord16le 
 
+e32 :: Endian -> Get Word32
 e32 endian = case endian of
                BigEndian -> getWord32be
                LittleEndian -> getWord32le 
@@ -40,11 +40,11 @@ type CR2Tag = (Word16, Word16, Word32, Word32)
 
 getCR2Tag :: Endian -> Get CR2Tag
 getCR2Tag endian = do
-  id <- e16 endian
+  id' <- e16 endian
   id_type <- e16 endian
   num <- e32 endian
   value <- e32 endian
-  return (id, id_type, num, value)
+  return (id', id_type, num, value)
 
 getCR2Tags :: Endian -> Get [CR2Tag]
 getCR2Tags endian = do
@@ -69,13 +69,13 @@ convertTagToAttribute (272, _, n, offset) = Just $ do
   make <- getLazyByteString $ fromInteger $ toInteger (n-1)
   return ("Model", lbs2s make)
 convertTagToAttribute _ = Nothing
--- Nothing passes here
-convertTagToAttribute (tag, 2, n, offset) = Just $ do
-  skip (fromInteger $ toInteger offset)
-  str <- getLazyByteString $ fromInteger $ toInteger (n-1)
-  return (show tag, lbs2s str)
-convertTagToAttribute (tag, _, n, offset) = Just $ do
-  return (show tag, "DEBUG")
+-- Nothing passes here.  The following can be used for debugging:
+--  convertTagToAttribute (tag, 2, n, offset) = Just $ do
+--    skip (fromInteger $ toInteger offset)
+--    str <- getLazyByteString $ fromInteger $ toInteger (n-1)
+--    return (show tag, lbs2s str)
+--  convertTagToAttribute (tag, _, n, offset) = Just $ do
+--    return (show tag, "DEBUG")
 
 getCR2Attributes :: Get [CR2Attribute]
 getCR2Attributes = do
